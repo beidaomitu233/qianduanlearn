fc33b1c3c79a13cf55bf22ca43b24486a444bb94
beidao <1206272713@qq.com>
2023-08-03 02:53:32 +0800

    vue2-2

--- vue2/Upgrade/vuedome2/src/App.vue
+++ vue2/Upgrade/vuedome2/src/App.vue
@@ -4,10 +4,11 @@
     <button @click="getdom2()">点击获取helloworld组件的DOM</button>
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>这是组件2 </h1>
-   
-    <hellotes :username="username" :userage="age" :userclass="zclass" />
-   <!-- 使用自定义事件进行传参  -->
-    <comp4 @sendata="sendataz()"/>
+
+    <hellotes :username="username" :userage="age"
+              :userclass="zclass" />
+    <!-- 使用自定义事件进行传参  -->
+    <comp4 @sendata="sendataz()" />
   </div>
 </template>
 
@@ -21,30 +22,38 @@ export default {
     hellotes,
     comp4
   },
-  data(){
+  data () {
     return {
-      targe:null,
-      username:'lisi',
-      age:18,
-      zclass:'三年级'
+      targe: null,
+      username: 'lisi',
+      age: 18,
+      zclass: '三年级'
     }
   },
-  
-  methods:{
-    getdom(){
+
+  methods: {
+    getdom () {
       console.log(this.$refs.btn1);
       console.log(this);
     },
-    getdom2() {
+    getdom2 () {
       console.log(this.$refs.hello2);
       console.log(HelloWorld);
-      
+
     },
-    sendataz(data){
+    sendataz (data) {
       console.log('接收到了组件4传来的数据');
       console.log(data);
     }
-  }
+  },
+  created () {
+    //第一个参数是自定义事件，第二个参数是回调函数接收的参数就是数据
+    this.$bus.$on('eventName1')
+    this.$bus.$emit()
+  },
+  beforeCreate () {
+    this.$bus.$off('eventName1')
+  },
 }
 </script>
 
--- vue2/Upgrade/vuedome2/src/main.js
+++ vue2/Upgrade/vuedome2/src/main.js
@@ -5,4 +5,8 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //安装时间总线
+  beforeCreate(){
+    this.prototype.$bus=this  //将$bus挂载到vue实例对象上
+  }
 }).$mount('#app')
